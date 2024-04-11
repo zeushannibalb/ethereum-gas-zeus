a52a5ce7ea94dec32ec54bff596c4a45141f2902
Zeus <162797332+zeushannibalb@users.noreply.github.com>
2024-04-11 22:34:16 +0800

    Update ethereumGas.js
    add comment

--- ethereumGas.js
+++ ethereumGas.js
@@ -1,5 +1,6 @@
 const { Web3 } = require('web3');
 
+// Get the gas of Ethereum
 async function ethereumGas(rpc_url) {
   const web3 = new Web3(rpc_url);
   const gasPrice = await web3.eth.getGasPrice();
